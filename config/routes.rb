bccd79ce43f3dc7f1d9000bd2e24ac845cc15f20
Mikhail Dyadchenko <mikhail_dyadchenko@mail.ru>
2024-12-26 02:57:41 +0300

    SubsForm

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Rails.application.routes.draw do
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
+  get "welcome/index"
+  get "welcome/about"
   # Defines the root path route ("/")
-  root "posts#index"
+  root "welcome#index"
 end
